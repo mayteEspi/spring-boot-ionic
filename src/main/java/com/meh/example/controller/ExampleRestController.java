b67f0e4210fc090f0e0470ffc2131df2a3b94fc2
mayteEspi <maytehermida@gmail.com>
2018-02-22 12:02:59 +0100

    remove imports

--- src/main/java/com/meh/example/controller/ExampleRestController.java
+++ src/main/java/com/meh/example/controller/ExampleRestController.java
@@ -6,8 +6,6 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/com/meh/example/controller/ExampleRestControllerIT.java
+++ src/test/java/com/meh/example/controller/ExampleRestControllerIT.java
@@ -4,26 +4,18 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.meh.example.model.UserModel;
-import com.meh.example.utils.SpringBootApiForIonicUtil;
 
 
 @RunWith(SpringRunner.class)
